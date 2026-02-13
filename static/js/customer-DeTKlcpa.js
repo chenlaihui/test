19a72b2ed1b7b4078cdbb98660329d8b56902df9
chenlaihui <vpma384@globalymc.com>
2026-02-13 13:23:08 +0800

    提交最新

---
+++ static/js/customer-DeTKlcpa.js
@@ -0,0 +1,1 @@
+import{aA as e}from"./index-CDHd4m-7.js";const s=t=>e.request({url:"/customer/list",method:"get",params:t}),o=t=>e.request({url:"/customer/detail",method:"get",params:t});export{o as a,s as g};
---
+++ static/js/paperPlane-wi0kQdUE.js
@@ -0,0 +1,1 @@
+import{a_ as s}from"./index-CDHd4m-7.js";const a=Symbol(),o=()=>s(a,null),e="/assets/paperPlane.svg";export{a as T,e as _,o as u};
